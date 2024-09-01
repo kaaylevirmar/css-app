479e595b079287a1a0d2ae915ff193b10a1aeec4
kaaylevirmar <kylevirmarmillendez@gmail.com>
2024-09-01 13:51:04 +0800

    update: added frontend validation

--- src/views/users/CreateUser.jsx
+++ src/views/users/CreateUser.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {useOutletContext} from 'react-router-dom';
 import { useFormik } from 'formik';
 import { useNavigate } from "react-router-dom";
+import { UserSignupSchema } from '../../middlewares/validation';
 
 const CreateUser = () => {
     const PORT = import.meta.env.VITE_PORT;
@@ -26,19 +27,21 @@ const CreateUser = () => {
             password: '',
             role: ''
         },
+        validationSchema: UserSignupSchema,
         onSubmit: (values) => {
             axios.post(`${baseUrl}:${PORT}/user-create`, values,
-                {
+                {   
                     'Content-Type': 'application/json',
                 }
-            ).then((res)=>{
+            ).then((res)=>{ 
+                console.log(res);
                 const { message, type } = res.data;
                 showFlashMessage(message,type);
                 navigate('/users');
-    
+                
             }).catch((err)=>{
-                console.log(err);
-                setErrorMessage(err.response.data.errors);
+                const {message, type} = err.response.data;
+                showFlashMessage(message,type); 
             })
         }
         
@@ -63,50 +66,38 @@ const CreateUser = () => {
                 <div className='w-full'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="firstName">First Name:</label>
                     <input className='border p-2 rounded-md border-gray-500 focus:outline-none focus:ring-2 w-full' id='firstName' type="text" value={formik.values.firstName} onChange={formik.handleChange} />
-                    {errorMessages && getErrorMessage('firstName').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}
+                    {formik.errors.firstName && formik.touched.firstName ? <div className='text-red-600 text-sm'>{ formik.errors.firstName }</div>: null}
                 </div>
                 <div className='w-full'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="lastName">Last Name:</label>
                     <input className='border p-2 rounded-md border-gray-500 focus:outline-none focus:ring-2 w-full' id='lastName' type="text" value={formik.values.lastName} onChange={formik.handleChange} />
-                    {errorMessages && getErrorMessage('lastName').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}
+                    {formik.errors.lastName && formik.touched.lastName ? <div className='text-red-600 text-sm'>{ formik.errors.lastName }</div> : null }
                 </div>
                 </div>
                 <div className='mb-6'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="email">Email:</label>
                     <input className='border p-2 rounded-md border-gray-500 focus:outline-none focus:ring-2 w-full' id='email' type="email" value={formik.values.email} onChange={formik.handleChange} />
-                    {errorMessages && getErrorMessage('email').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}
+                    {formik.errors.email && formik.touched.lastName ? <div className='text-red-600 text-sm'>{ formik.errors.email }</div> : null}
                 </div>
                 <div className='mb-6'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="username">Username: </label>
                     <input className='border p-2 rounded-md border-gray-500 focus:outline-none focus:ring-2 w-full' id='username' type="text" value={formik.values.username} onChange={formik.handleChange} />
-                    {errorMessages && getErrorMessage('username').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}    
+                    {formik.errors.username && formik.touched.username ? <div className='text-red-600 text-sm'>{ formik.errors.username }</div>: null}
                 </div>
                 <div className='mb-6'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="password">Password:</label>
                     <input className='border p-2 rounded-md border-gray-500 focus:outline-none focus:ring-2 w-full' id='password' type="password" value={formik.values.password} onChange={formik.handleChange} />
-                    {errorMessages && getErrorMessage('password').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}
+                    {formik.errors.password && formik.touched.password ? <div className='text-red-600 text-sm'>{ formik.errors.password }</div>: null}
                 </div>
                 <div className='mb-6'>
                     <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor="role">Role:</label>
                     <select className='w-1/2 p-2 rounded border  border-gray-700' name="role" id="role" value={formik.values.role} onChange={formik.handleChange}>
-                        <option selected={true} value="" disabled>-</option>
+                        <option value="">-</option>
                     {roles.map((role,index)=>{
                         return <option key={index} value={role.value}>{role.option}</option>
                     })}
                     </select>
-                    {errorMessages && getErrorMessage('password').map((value,key)=>{
-                        return(<div key={key} className='text-red-600 text-sm'>{ value.message }</div>);
-                    })}
+                    {formik.errors.role && formik.touched.role ? <div className='text-red-600 text-sm'>{ formik.errors.role }</div>: null}
                 </div>
                 <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='submit'>Submit</button>
             </form>
